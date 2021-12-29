7df3ff1ea8b9a4c5f2072f594f5ff66df1684f88
satheesh nayaka k a <sathishadipad1998@gmail.com>
2021-12-29 12:39:59 +0530

    passed

---
+++ assignments/assignment_2/index.js
@@ -0,0 +1,11 @@
+const http=require('http');
+var fs=require('fs')
+http.createServer((req,res)=>{
+    fs.readFile('index.html',(err,data)=>{
+        res.writeHead(200,{'content-type':'text/html'});
+        res.write(data);
+        return res.end();
+    })
+}
+)
+.listen(3000);
